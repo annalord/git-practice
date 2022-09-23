2cbeed1410da999f51c7720dfbf66d6e7cfb3667
Annie <annie.gallagher7@gmail.com>
2022-09-23 15:23:37 -0600

    fixed weird merge problem to return to print and return max

--- function_d.py
+++ function_d.py
@@ -1,14 +1,9 @@
 def max_value(numbers):
-<<<<<<< HEAD
-    return max(numbers)
-    
-=======
     """ This function returns the largest number
         in the list.
     """
     print("hello there")
     return max(numbers)
->>>>>>> 9db3971e9fd0b6e9c818a63669c9613b7c6da8a6
 
 
 if __name__ == "__main__":
